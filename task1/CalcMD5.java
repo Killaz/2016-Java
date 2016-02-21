601b7a0c6b20eb51ed9671fde385d997a0926f95
Killaz <Kanootoko@gmail.com>
2016-02-21 19:36:19 +0300

    Task1: simple, but doesen't work

--- task1/CalcMD5.java
+++ task1/CalcMD5.java
@@ -1,26 +1,51 @@
 import java.io.*;
 import java.util.Scanner;
-import java.DigestUtils.MD5;
+import java.math.BigInteger;
+import javax.xml.bind.DatatypeConverter;
+import java.security.*;
+import java.nio.file.Paths;
+import java.nio.file.Path;
+import java.nio.file.Files;
 
 class CalcMD5 {
 	public static void main (String[] args) throws Exception {
 		Scanner in, file;
-		String name = "", strOfFile;
+		String name = "";
 		if (args.length > 1)
 			in = new Scanner(args[1]);
 		else
 			in = new Scanner(new FileReader("input.txt"));
 		while (in.hasNext()) {
+			byte strOfFile[] = new byte[4], tmp[];
+			char c;
+			int len = 0, maxlen = 4;
 			name = in.next();
 			file = new Scanner(new FileReader(name));
 			file.useDelimiter("");      // read char-s
-			strOfFile = "";
-			while (file.hasNext())
-				strOfFile += file.next();
+			while (file.hasNext()) {
+				c = in.next().charAt(0);
+				if (len == maxlen) {
+					tmp = strOfFile;
+					strOfFile = new byte[maxlen * 2];
+					for (int i = 0; i < len; i++)
+						strOfFile[i] = tmp[i];
+					maxlen *= 2;
+					tmp = null;
+				}
+				System.out.println("Lol)) " + c);
+				strOfFile[len] = (byte) c;
+				len++;
+				
+			}
 			file.close();
-			System.out.println(DigestUtils.md5Hex(strOfFile));
-//			System.out.println(strOfFile);
-			
+
+			MessageDigest md = MessageDigest.getInstance("MD5");
+/*			Path path = Paths.get(sc.nextLine());
+			byte[] data = Files.readAllBytes(path);
+			md.update(data, 0, data.length); */
+			md.update(strOfFile, 0, strOfFile.length);
+			System.out.println(DatatypeConverter.printHexBinary(md.digest()));
 		}
+		in.close();
 	}
 }
